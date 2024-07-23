1d23f626ed389a1f6e2e63a29a1f971adba933ae
rytst <r.saito0111@gmail.com>
2024-07-24 08:05:27 +0900

    add plot

--- gradient/20240724/regression.py
+++ gradient/20240724/regression.py
@@ -1,5 +1,6 @@
 import torch
-
+# import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 def predict(x, W, b):
     y = x @ W + b
@@ -14,6 +15,8 @@ def mean_squared_error(x0, x1):
 
 
 
+
+
 def main():
     # toy dataset
     torch.manual_seed(0)
@@ -23,7 +26,8 @@ def main():
     W = torch.zeros(1, 1, requires_grad=True)
     b = torch.zeros(1, requires_grad=True)
     
-
+    
+    # training
     lr = 0.1
     iters = 100
 
@@ -31,12 +35,13 @@ def main():
     for i in range(iters):
         y_hat = predict(x, W, b)
         loss = mean_squared_error(y, y_hat)
+        # loss = F.mse_loss(y, y_hat)
 
 
         loss.backward()
 
-        W.data -= W.grad.data 
-        b.data -= b.grad.data
+        W.data -= lr * W.grad.data 
+        b.data -= lr * b.grad.data
 
         W.grad.zero_()
         b.grad.zero_()
@@ -44,6 +49,21 @@ def main():
 
         if i % 10 == 0:
             print(loss.item())
+
+    print(loss.item())
+    print('===========================')
+    print('W = ', W.item())
+    print('b = ', b.item())
+
+
+    plt.scatter(x, y)
+    plt.plot(x, W.item() * x + b.item(), color='red')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.savefig('regression.png')
+
+
+
 
 
 
